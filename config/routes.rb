9beb00d5c2d513292c393f9b90bb92ebbebf30e8
Alexander Presber <post@momolog.info>
2012-08-07 20:59:25 +0200

    add init page

--- config/routes.rb
+++ config/routes.rb
@@ -51,7 +51,7 @@ Nanocloud::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
