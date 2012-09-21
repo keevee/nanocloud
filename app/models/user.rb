bf9ea377eb7f64cc22b03148642b794722d76a77
azini <aazadi@gmail.com>
2012-09-21 20:35:36 +0200

    more frontend stuff make the registration open

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
-         :recoverable, :trackable
+         :recoverable, :trackable, :registerable
   #devise :database_authenticatable, :registerable,
   #       :recoverable, :rememberable, :trackable, :validatable
 
