66ba5a56de259456af4422b6e2795d5af4258384
azini <aazadi@gmail.com>
2012-09-24 13:47:36 +0200

    fix the admin for setting the user websites hide unimportant columns fix the compile form

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,17 @@
 ActiveAdmin.register User do
-  
+  index do
+    column :email
+    column :website
+    default_actions
+  end
+
+  filter :email
+
+  form do |f|
+    f.inputs "Admin Details" do
+      f.input :email
+      f.input :website
+    end
+    f.buttons
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   #       :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :website_id
   # attr_accessible :title, :body
 
   belongs_to :website
