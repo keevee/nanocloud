cbd0677e509821d8aa2825789d12f93b3cc0c440
azini <aazadi@gmail.com>
2012-09-24 14:04:56 +0200

    fix the signout link

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // GO AFTER THE REQUIRES BELOW.
 //
 // Loads all Bootstrap javascripts
-//= require bootstrap
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
 //= require_tree .
