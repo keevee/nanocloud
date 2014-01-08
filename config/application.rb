864b79ddb7a225c64edb0801b608416b7466ff63
Alexander Presber <post@momolog.info>
2014-01-08 23:31:47 +0100

    https://devcenter.heroku.com/articles/rails-asset-pipeline#troubleshooting

--- config/application.rb
+++ config/application.rb
@@ -61,5 +61,8 @@ module Nanocloud
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # https://devcenter.heroku.com/articles/rails-asset-pipeline#troubleshooting
+    config.assets.initialize_on_precompile = false
   end
 end
