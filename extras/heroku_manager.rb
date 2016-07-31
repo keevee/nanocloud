a290fcca1f611a2c1f2a1e1174cd8b326daea3b4
Alexander Presber <post@momolog.info>
2016-07-31 21:54:17 +0200

    output worker-count

--- extras/heroku_manager.rb
+++ extras/heroku_manager.rb
@@ -4,10 +4,14 @@ class HerokuManager
   end
 
   def get_workers
-    heroku.get_ps(ENV['APP_NAME']).body.count { |p| p["process"] =~ /worker\.\d?/ }
+    p ">>> get_workers"
+    worker_count = heroku.get_ps(ENV['APP_NAME']).body.count { |p| p["process"] =~ /worker\.\d?/ }
+    p ">>> #{worker_count}"
+    worker_count
   end
 
   def set_workers(count)
+    p ">>> set_workers #{count}"
     heroku.post_ps_scale(ENV['APP_NAME'], 'worker', count)
   end
 end 
